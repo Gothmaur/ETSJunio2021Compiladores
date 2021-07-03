d86d04fa2547f6426fcad0909de49abf56d7d6d9
Gothmaur <78580948+Gothmaur@users.noreply.github.com>
2021-07-02 23:00:31 -0500

    Inclusión de la clase base de las normas de producción

---
+++ LL1/Produccion.cpp
@@ -0,0 +1,10 @@
+#include "Produccion.hpp"
+
+/**
+ * Constructor
+ * **/
+Produccion::Produccion(char simb, std::string produccion)
+{
+	Simbolo = simb;
+	prod = produccion;
+}
---
+++ LL1/Produccion.hpp
@@ -0,0 +1,15 @@
+#ifndef PRODUCCION_HPP_
+#define PRODUCCION_HPP_
+
+#include <String>
+
+class Produccion {
+public:
+
+  char Simbolo;
+  std::string prod;
+
+  Produccion(char simb, std::string produccion);
+};
+
+#endif // PRODUCCION_HPP_
